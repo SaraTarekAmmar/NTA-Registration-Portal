6ac44c313cf84bb520058abff7cd51b3d1ca1af0
SaraTarekAmmar <91714658+SaraTarekAmmar@users.noreply.github.com>
2026-06-24 12:31:18 +0300

    feat(coordinator): add interview navigation

--- coordinator/js/coordinator-layout.js
+++ coordinator/js/coordinator-layout.js
@@ -24,6 +24,8 @@
     dashboard: ic('<path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>'),
     attendance: ic('<path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"/>'),
     permissions: ic('<path d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>'),
+    interviews: ic('<path d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857M15 7a3 3 0 11-6 0 3 3 0 016 0z"/>'),
+    evaluations: ic('<path d="M9 11l3 3L22 4"/><path d="M21 12v7a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2h11"/>'),
     logout: ic('<path d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>'),
   };
 
@@ -45,6 +47,10 @@
 
     var nav = '<div class="nta-sidebar__nav-lbl">الرئيسية</div>' +
       navItem("coordinator-dashboard.html", "dashboard", ICONS.dashboard, "لوحة التحكم", activePage) +
+      '<div class="nta-sidebar__divider"></div>' +
+      '<div class="nta-sidebar__nav-lbl">المقابلات</div>' +
+      navItem("coordinator-interviews.html", "interviews", ICONS.interviews, "طابور المقابلات", activePage) +
+      navItem("coordinator-evaluations.html", "evaluations", ICONS.evaluations, "متابعة التقييمات", activePage) +
       '<div class="nta-sidebar__divider"></div>' +
       '<div class="nta-sidebar__nav-lbl">الإدارة</div>' +
       navItem("coordinator-attendance.html", "attendance", ICONS.attendance, "الحضور والغياب", activePage) +
